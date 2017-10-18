fbbcef2cccd7073200caed77aaf1a88e7b3d77d5
Franckensteyn <y.assiine@hotmail.fr>
2017-10-18 15:42:46 +0200

    mise en place des articles et des différentes catégories

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\AppBundle;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class DefaultController extends Controller
 {
@@ -17,9 +19,36 @@ class DefaultController extends Controller
 
         $articles = $em->getRepository('AppBundle:Article')->findAll();
         $sections = $em->getRepository('AppBundle:Section')->findAll();
+
         return $this->render('default/index.html.twig', array(
             'articles' => $articles,
-            'sections' => $sections,
+            'sections' => $sections
+        ));
+    }
+    /**
+     * @Route("/section/{id}", name="sections")
+     */
+    public function sectionAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // pour récupérer le titre
+        $titre_section = $em->getRepository('AppBundle:Section')->find($id);
+        // les articles
+        $repository = $em->getRepository('AppBundle:Article');
+        $articles = $repository->createQueryBuilder('a')
+            ->innerJoin('a.section', 's')
+            ->where('s.id = :idactu')
+            ->setParameter('idactu', $id)
+            ->getQuery()->getResult();
+        //dump($articles);
+        // menu
+        $sections = $em->getRepository('AppBundle:Section')->findAll();
+
+        return $this->render('default/section.html.twig', array(
+            'titre' => $titre_section,
+            'articles' => $articles,
+            'sections' => $sections
         ));
     }
 }
